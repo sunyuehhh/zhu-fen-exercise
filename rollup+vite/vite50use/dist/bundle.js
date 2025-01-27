86db709d1c716b0d0a2022f2d9f85da134364c13
孙悦 <11075393+sinking-fallen-leaves@user.noreply.gitee.com>
2025-01-27 20:43:34 +0800

    替换

---
+++ rollup+vite/vite50use/dist/bundle.js
@@ -0,0 +1,3 @@
+// src/main.js
+import { createApp } from "D:/xuexishipin/exercise/zhu-fen-exercise/rollup+vite/vite50use/node_modules/vue/dist/vue.runtime.esm-bundler.js";
+console.log(createApp, "main");
