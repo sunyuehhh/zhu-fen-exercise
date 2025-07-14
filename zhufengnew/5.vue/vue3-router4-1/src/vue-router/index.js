80f524292f8e050f1d06defdec54402633ebf983
孙悦 <11075393+sinking-fallen-leaves@user.noreply.gitee.com>
2025-07-14 21:56:41 +0800

    vue-router源码:钩子

--- zhufengnew/5.vue/vue3-router4-1/src/vue-router/index.js
+++ zhufengnew/5.vue/vue3-router4-1/src/vue-router/index.js
@@ -96,6 +96,21 @@ const START_LOCATION_NORMALIZED={//初始化路由系统中得默认参数
 }
 
 
+function useCallback(){
+  const handlers=[]
+  function add(handler){
+    handlers.push(handler)
+
+
+  }
+
+  return {
+    add,
+    list:()=>handlers
+  }
+
+}
+
 
 function createRouter(options){
   console.log(options,'options')
@@ -110,6 +125,13 @@ function createRouter(options){
   // 后续改变这个数据得value  就可以更新视图
   const currentRoute=shallowRef(START_LOCATION_NORMALIZED);//obj.value=响应式数据
   // 将数据用计算属性   再次包裹
+
+
+
+  // 全局钩子
+  const beforeGuards=useCallback()
+  const beforeResolveGuards=useCallback()
+  const afterGuards=useCallback()
 
   function resolve(to){//to="/"  to={path:'/'}
     if(typeof to==='string'){
@@ -149,14 +171,127 @@ function createRouter(options){
   }
 
 
+  function extractChangeRecords(to,from){
+    const leavingRecords=[]
+    const updatingRecords=[]
+    const enteringRecords=[]
+    
+    for(let i=0;i<length;i++){
+      const recordFrom=from.matched[i]
+      if(recordFrom){
+        // 去得和来的都有  那么就是要更新
+        if(to.matched.find(record=>record.path==recordFrom.path)){
+          updatingRecords.push(recordFrom)
+        }else{
+          leavingRecords.push(recordFrom)
+        }
+      }
+
+      const recordTo=to.matched[i]
+      if(recordTo){
+        if(!from.matched.find(record=>record.path===recordTo.path)){
+          enteringRecords.push(recordTo)
+        }
+
+      }
+    }
+
+    return [leavingRecords,updatingRecords,enteringRecords]
+  }
+
+
+  function guardToPromise(guard,to,from,record){
+    return ()=>new Promise((resolve)=>{
+      const next=()=>resolve()
+
+      const guardReturn=guard.call(record,to,from,next)//用户调next  这个promise就成功了
+
+      // 如果不调用next 最终也会调用next  用户可以不调用next方法
+      return Promise.resolve(guardReturn).then(next)
+
+
+    })
+
+
+  }
+
+  function extractComponentsGuards(matched,guardType,to,from){
+    const guards=[]
+    for(const record of matched){
+      let rawComponent=record.components.default
+      const guard=rawComponent[guardType]
+
+      // 我需要将钩子  全部串联再一起  promise
+      guard&&guards.push(guardToPromise(guard,to,from,record))
+    }
+
+    return guards
+
+  }
+
+
+  // promise得组合函数
+  // 按顺序执行  串联起来   [fn()=>promise,fn=>promise]
+  function runGuardQueue(guards){
+    return guards.reduce((promise,guard)=>promise.then(()=>guard()),Promise.resolve())
+
+  }
+
+  function navigate(to,from){
+    // 再做导航得时候   我要知道哪个组件时进入  哪个组件时离开得  还要知道哪个组件时更新得
+
+    const [leavingRecords,updateRecords,enteringRecords]=extractChangeRecords(to,from)
+
+    // 我离开得时候  需要从后往前   /home/a  ->about
+   let guards= extractComponentsGuards(leavingRecords.reverse(),
+  'beforeRouteLeave',to,from)
+
+  return runGuardQueue(guards).then(()=>{
+    guards=[]
+    for(const guard of beforeGuards.list()){
+      guards.push(guardToPromise(guard,to,from,guard))
+    }
+
+    return runGuardQueue(guards)
+  }).then(()=>{
+    guards=extractComponentsGuards(leavingRecords,
+  'beforeRouteUpdate',to,from)
+
+  return runGuardQueue(guards)
+  }).then(()=>{
+    guards=[]
+
+    for(const record of to.matched){
+      if(record.beforeEnter){
+      guards.push(guardToPromise(record.beforeEnter,to,from,record))
+      }
+    }
+
+    return runGuardQueue(guards)
+  }).then(()=>{
+        guards=extractComponentsGuards(enteringRecords,
+  'beforeRouteEnter',to,from)
+
+  return runGuardQueue(guards)
+  })
+
+  }
+
   function pushWithRedirect(to){//通过路径匹配到对应得记录  更新currentRoute
     const targetLocation=resolve(to)
     const from=currentRoute.value
 
-    console.log(targetLocation,from,'&&&&&&')
+    // console.log(targetLocation,from,'&&&&&&')
 
-    finalizeNavigation(targetLocation,from)
-    // 根据是不是第一次 来决定是push  还是replace
+    // finalizeNavigation(targetLocation,from)
+    // // 根据是不是第一次 来决定是push  还是replace
+
+    navigate(targetLocation,from).then(()=>{
+      return finalizeNavigation(targetLocation,from)
+    }).then(()=>{
+      // 当导航切换完毕后执行afterEach
+      for(const guard of afterGuards.list()) guard(to,from)
+    })
 
   }
 
@@ -167,6 +302,9 @@ function push(to){
 
   const router={
     push,
+    beforeEach:beforeGuards.add,
+    afterEach:afterGuards.add,//可以注册多个  所以是一个发布订阅模式
+    beforeResolve:beforeResolveGuards.add,
     replace(){},
 
     install(app){//路由得核心就是  页面切换  重新渲染
